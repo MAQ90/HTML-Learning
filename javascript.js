10fd245c80043b177c23618bfcc2685b8f87ea79
Muhammad Aamir <bone.authentic@gmail.com>
2016-10-29 21:01:11 +0500

    javascript lab

---
+++ javascript.js
@@ -0,0 +1,32 @@
+// function studentId(roll) {
+//     alert("student Id "+ roll);
+// }
+
+// studentId(9035);
+
+// function cost(n1,n2,n3) {
+//     var ans = ("Cost "+(n1 + n2)*n3 + " of Tea")
+//       if (isNaN(ans)){
+//         alert("please put a valid information");
+        
+//       }else{
+//             alert (ans);
+//         }        
+//     }
+
+// var abc = prompt("first cost");
+// var a = parseInt(abc);
+// var xyz = prompt("Scend cost");
+// var b = parseInt(xyz);
+// var zyx = prompt("Third cost");
+// var c = parseInt(zyx);
+
+// cost(a,b,c);
+
+var recipe1 = ["egg", "milk", "floor"];
+var recipe2 = ["egg", "oil", "salt"];
+var recipe3 = ["sugar", "milk", "floor"];
+
+function myRecipe(recipe1,recipe2,recipe3){
+    if 
+}
